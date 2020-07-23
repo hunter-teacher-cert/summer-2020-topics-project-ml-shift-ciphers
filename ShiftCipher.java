13df9baef234bf2c0806b8e1a370b0b10dd6c665
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-23 11:02:53 -0400

    added outline

---
+++ ShiftCipher.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class ShiftCipher {
+  // Attributes - encoded string provided by the user.
+  private String input;
+
+/*
+  Gets the input string from the user
+*/
+  public String getInput(){
+    return "Hello World";
+  }
+
+/*
+  Turns a string of various cases to lowercase
+*/
+  public String lowercase (String s){
+    return "Hello World"; // bye-bye compiler
+  }
+
+/*
+  Calculates frequency of a character in a given string
+*/
+  public int[] frequencyOf (String s){
+    return null; // bye-bye compiler
+  }
+
+/*
+  Shifts all letters in a string "i" places to the right.
+*/
+  public String shiftRight (int i){
+      return "Hello World"; // bye-bye compiler
+  }
+
+/*
+  Shifts all letters in a string "i" places to the left.
+*/
+  public String shiftLeft (int i){
+    return "Hello World"; // bye-bye compiler
+  }
+
+  public static void main (String[] args){
+
+  }
+
+}
